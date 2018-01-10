20d55fd782ae892bfe28365e759cdb54b8fa097d
Yervand Aghababyan <yervand.aghababyan@gmail.com>
2018-01-10 20:51:02 +0400

    Added verbose logging to the .travis/build.sh build script
    Signed-off-by: Yervand Aghababyan <yervand.aghababyan@gmail.com>

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-
 if [ $TRAVIS_BRANCH == 'develop' ]
 then
+    echo "Running develop branch build and analysis. Snapshots will be published. All issues/stats will be saved to Sonar database."
     mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar -B \
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.organization=sfl \
     -Dsonar.login=$SONARCLOUD_KEY
 elif [ "$TRAVIS_PULL_REQUEST" != "false" ]
 then
+    echo "Running Github PR build and analysis. Sonar will run in preview mode."
     mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar -B \
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.organization=sfl \
@@ -17,5 +18,6 @@ then
     -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
     -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH_TOKEN
 else
+    echo "Running build without sonar analysis."
     mvn clean verify -B
 fi
