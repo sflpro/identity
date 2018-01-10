1176647e4690bfa88ea20b167ab83dfc7b1b061d
Yervand Aghababyan <yervand.aghababyan@gmail.com>
2018-01-10 20:38:18 +0400

    CI improvements
    * Configured pull request analysis for Github/Sonar
    * Improvements to travis build itself
    
    Signed-off-by: Yervand Aghababyan <yervand.aghababyan@gmail.com>

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
 
-if [ $TRAVIS_BRANCH == 'develop' ] || [ $TRAVIS_BRANCH == 'master' ]
+if [ $TRAVIS_BRANCH == 'develop' ]
 then
     mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar -B \
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.organization=sfl \
     -Dsonar.login=$SONARCLOUD_KEY
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar -B \
+    -Dsonar.host.url=https://sonarcloud.io \
+    -Dsonar.organization=sfl \
+    -Dsonar.login=$SONARCLOUD_KEY \
+    -Dsonar.github.repository=sflpro/identity \
+    -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+    -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH_TOKEN
 else
     mvn clean verify -B
 fi
