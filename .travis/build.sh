56b1bbdfa327dbca3989c92390eff6371f8ce131
Yervand Aghababyan <yervand.aghababyan@gmail.com>
2018-01-25 20:49:29 +0400

    Configuring dockerhub auth for travis builds

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,10 +3,19 @@
 if [ "$TRAVIS_BRANCH" == "develop" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
     echo "Running develop branch build and analysis. Snapshots will be published. All issues/stats will be saved to Sonar database."
-    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar -B \
+    mvn -P snapshot clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar -B \
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.organization=sfl \
     -Dsonar.login=$SONARCLOUD_KEY
+elif [ $TRAVIS_BRANCH == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]
+then
+    init_central_auth
+    echo "Running master branch build and analysis. Release artifacts will be published.. Sonar run will be skipped."
+    mvn -P release clean org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar -B \
+    -Dsonar.host.url=https://sonarcloud.io \
+    -Dsonar.organization=sfl \
+    -Dsonar.login=$SONARCLOUD_KEY \
+    -Dgpg.passphrase=$TRAVIS_GPG_KEY_PASS
 elif [ "$TRAVIS_PULL_REQUEST" != "false" ]
 then
     echo "Running Github PR build and analysis. Sonar will run in preview mode."
