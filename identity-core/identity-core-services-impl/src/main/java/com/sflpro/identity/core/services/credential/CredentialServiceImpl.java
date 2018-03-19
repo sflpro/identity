d616167f6f80694c58c13f255a03097187354cdc
Ani Janamyan <ani-91@inbox.ru>
2018-03-19 17:23:21 +0400

    remove import

--- identity-core/identity-core-services-impl/src/main/java/com/sflpro/identity/core/services/credential/CredentialServiceImpl.java
+++ identity-core/identity-core-services-impl/src/main/java/com/sflpro/identity/core/services/credential/CredentialServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.time.LocalDateTime;
 import java.util.List;
 
 /**
